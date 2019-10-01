248e6debaf97a4b91b190faf1de7c16c733b9cb2
johnk005297 <45561283+johnk005297@users.noreply.github.com>
2019-10-01 20:24:54 +0300

    Add files via upload
    Выгрузка пользователей из системы

---
+++ select_users.sql
@@ -0,0 +1,18 @@
+select l.LoginName Users, 
+case
+	when l.passwordHash = 'vFRQPemIV0HURsKLTF+tO+8uBkI=' then 'qqq111'
+	when l.passwordHash = 'ws7hfnvigyEAkN113/rQkzTEBGg=' then '123'
+	when l.passwordHash = 'CZInYdVykek6IMCEw9ql3DMQMG0=' then 'demo321'
+	when l.passwordHash = 'LA4qU++A26qKuOlXFZVrTl1itpQ=' then 'www222'
+	when l.passwordHash = '3q/vHsQzO5UwbOTSdg7AJw75q0c=' then 'admin123'
+	when l.passwordHash = '3QHrhRk2AGx8op4NtDTzDZoTGzs=' then 'Admdemo'
+	else l.passwordHash
+end passwordOrHash, Discriminator, 
+case
+	when u.Deleted = 1 then 'DELETED'
+	when u.Blocked = 1 then 'BLOCKED'
+	else 'active'
+end Status
+ from boardmaps_Logins l 
+ inner join boardmaps_Users u
+ on u.Id = l.UserId;
