72ee6ce718fc6f2890473fffa4c9360cf98d1a60
johnk005297 <45561283+johnk005297@users.noreply.github.com>
2019-11-05 21:18:46 +0300

    Update get_users_with_admHolding_creds.sql

--- get_users_with_admHolding_creds.sql
+++ get_users_with_admHolding_creds.sql
@@ -4,26 +4,27 @@
 
 select l.loginname HoldingAdmins, 
 case 
-when l.passwordHash = 'vFRQPemIV0HURsKLTF+tO+8uBkI=' then 'qqq111'
-when l.passwordHash = 'CZInYdVykek6IMCEw9ql3DMQMG0=' then 'demo321'
-when l.passwordHash = 'LA4qU++A26qKuOlXFZVrTl1itpQ=' then 'www222'
-when l.passwordHash = '3q/vHsQzO5UwbOTSdg7AJw75q0c=' then 'admin123'
-when l.passwordHash = '3QHrhRk2AGx8op4NtDTzDZoTGzs=' then 'Admdemo'
-else l.passwordHash
+  when l.passwordHash = 'vFRQPemIV0HURsKLTF+tO+8uBkI=' then 'qqq111'
+  when l.passwordHash = 'CZInYdVykek6IMCEw9ql3DMQMG0=' then 'demo321'
+  when l.passwordHash = 'LA4qU++A26qKuOlXFZVrTl1itpQ=' then 'www222'
+  when l.passwordHash = '3q/vHsQzO5UwbOTSdg7AJw75q0c=' then 'admin123'
+  when l.passwordHash = '3QHrhRk2AGx8op4NtDTzDZoTGzs=' then 'Admdemo'
+  else l.passwordHash
 end passwordHash,
   l.Discriminator,
 case
-when u.Deleted = 1 then 'DELETED'
-when u.Blocked = 1 then 'BLOCKED'
-else 'active'
+  when u.Deleted = 1 then 'DELETED'
+  when u.Blocked = 1 then 'BLOCKED'
+  else 'active'
 end Status
 from company_Logins l
 inner join company_Users u
 on l.UserId = u.Id
-inner join (select a.UserId from company_AccessControlList a
-where a.RoleId =
-( select id from company_RoleModel_RoleTypes 
-where BuiltinType = 'HoldingAdministrator' and HoldingId is not null ) and a.Deleted = 0 group by a.UserId ) r
-on r.UserId = l.UserId
+inner join 
+  (select a.UserId from company_AccessControlList a
+    where a.RoleId =
+      (select id from company_RoleModel_RoleTypes 
+        where BuiltinType = 'HoldingAdministrator' and HoldingId is not null ) and a.Deleted = 0 group by a.UserId ) r
+        on r.UserId = l.UserId
 order by Status;
 
